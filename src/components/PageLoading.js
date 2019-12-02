cc24dcc2268b723e1e270397edae4561ca0440f4
Nataly Silva <natalysilva@MBP-de-Nataly.lan>
2019-12-02 12:04:33 -0600

    Add api to find our data Add Loader component

---
+++ src/components/PageLoading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/loading.css'
+
+function PageLoading(){
+  return(
+    <div className="PageLoading">
+      'Loading...'
+    </div>
+  ) 
+}
+
+export default PageLoading;
